1bd81c8c0a0dddcf732d5e8f7d737c882d5140de
jvestolas <josephvictorestolas@gmail.com>
2019-03-24 19:08:57 -0700

    added floor and handlers for wasd camera movement

---
+++ src/client/config/keycodes.js
@@ -0,0 +1,24 @@
+export const keyCodes = {
+  left: {
+    key: 'a',
+    code: 65,
+    direction: 'left',
+  },
+  right: {
+    key: 'd',
+    code: 68,
+    direction: 'right',
+  },
+  up: {
+    key: 'w',
+    code: 87,
+    direction: 'up',
+  },
+  down: {
+    key: 's',
+    code: 83,
+    direction: 'down',
+  },
+};
+
+export default keyCodes;
